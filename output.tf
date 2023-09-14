fdf7097fd831d2d9fc0a3f45e7f8e3ff1919f695
hchenxa <huichen@redhat.com>
2023-09-14 17:28:29 +0800

    add commands sample
    Signed-off-by: hchenxa <huichen@redhat.com>

--- output.tf
+++ output.tf
@@ -6,3 +6,9 @@ output "node_ssh" {
   value       = "ssh -i ${var.ssh_private_key_path} ec2-user@${aws_instance.microshift_node.public_ip}"
   description = "Public IP of node (for SSH access)"
 }
+
+# Output the kubeconfig
+output "kubeconfig_file" {
+  value       = "ssh -i ${var.ssh_private_key_path} ec2-user@${aws_instance.microshift_node.public_ip} sudo cat /var/lib/microshift/resources/kubeadmin/${var.cluster_name}.${var.aws_base_dns_domain}/kubeconfig"
+  description = "The commands can get the kubeconfig file"
+}
